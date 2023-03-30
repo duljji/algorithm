fa17d8c0559954a56b3ac1b08a727c61c37329ea
duljji <108582424+duljji@users.noreply.github.com>
2023-03-30 15:29:58 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 452 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -2,7 +2,7 @@ N = int(input())
 
 num = input().split()
 
-for i in range(len(num)-1, -1, -1):
+for i in range(N-1, -1, -1):
     for j in range(i):
         if int(num[j] + num[j+1]) < int(num[j+1] + num[j]) :
             num[j], num[j+1] = num[j+1], num[j]
