b252cb1b3eda14373c7e952b32d66aa63ab74163
duljji <108582424+duljji@users.noreply.github.com>
2023-04-05 16:47:14 +0900

    [Gold IV] Title: 최단경로, Time: 688 ms, Memory: 68516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,34 +1,31 @@
-import sys
+import heapq
+import sys 
+
 input = sys.stdin.readline
-def dijkstra(s, V):
-    U = [0] * (V+1)
-    D[s] = 0
-    for e, w in adj[s] :
-        D[e] = min(D[e], w)
-    U[s] = 1
-    for _ in range(V+1):
-        minV = INF
-        t = 0
-        for i in range(V+1):
-            if U[i] == 0 and minV > D[i] :
-                minV = D[i]
-                t = i
-        U[t] = 1
-        for e, w in adj[t] :
-            D[e] = min(D[e], D[t]+w)
+INF = int(1e9)
+v,e = map(int,input().split())
+start = int(input())
+gragh = [[] for i in range(v+1)]
+distance = [INF]*(v+1)
 
+for _ in range(e):
+    u1,u2,w = map(int,input().split())
+    gragh[u1].append((u2,w))
+    
+q = []
+heapq.heappush(q,(0,start))
+distance[start] = 0
+while q:
+    dist, now = heapq.heappop(q)
+    if dist>distance[now]:
+        continue
+    for i in gragh[now]:
+        cost = dist + i[1]
+        if cost < distance[i[0]]:
+            distance[i[0]] = cost
+            heapq.heappush(q,(cost,i[0]))
 
-V, E = map(int, input().split())
-S = int(input())
-adj = [[] for _ in range(0, V+1)]
-for _ in range(E):
-    s, e, w = map(int, input().split())
-    adj[s].append((e, w))
-INF = 1000000000
-D = [INF] * (V+1)
-dijkstra(S, V)
-for i in range(1, V+1):
-    if D[i] == INF:
-        print('INF')
-    else :
-        print(D[i])
+for i in range(v+1):
+    if i ==0 : continue
+    if distance[i] == INF: print('INF')
+    else : print(distance[i])
