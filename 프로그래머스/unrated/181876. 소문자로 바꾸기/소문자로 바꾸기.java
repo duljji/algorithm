96f9df8bbf35e3e087e0b4fcdbf1b1ed6514d03a
duljji <108582424+duljji@users.noreply.github.com>
2023-04-24 13:19:19 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 11.26 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        Scanner sc = new Scanner(System.in);
+        String answer = myString.toLowerCase();
+        
+        return answer;
+    }
+}
