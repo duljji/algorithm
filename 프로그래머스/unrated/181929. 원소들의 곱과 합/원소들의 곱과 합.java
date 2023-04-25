799225cc572f1b5d2aae057a24871f788001311e
duljji <108582424+duljji@users.noreply.github.com>
2023-04-25 15:41:17 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int hab = 0;
+        int gob = 1;
+        for (int i=0; i<num_list.length; i++){
+            hab += num_list[i];
+            gob *= num_list[i];
+            
+        }
+        if (hab*hab > gob){
+            answer =1;
+        } 
+        return answer;
+    }
+}
